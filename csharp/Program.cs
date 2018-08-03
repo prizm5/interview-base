35bc81f23ffda19bb515ee9e7c2c3fdf4a881f75
Nils Creque <nils.creque@smartstream-stp.com>
2018-08-03 14:21:22 +0000

    last of th edits

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,12 +1,9 @@
 using System;
 
-namespace consoleapp
-{
-  class Program
-  {
-      static void Main(string[] args)
-      {
-          Console.WriteLine("Hello World!");
-      }
+namespace consoleapp {
+  class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+    }
   }
 }
