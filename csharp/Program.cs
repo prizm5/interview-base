a08398db370ab9bc13d5f47c114a7f388f2070a6
Nils Creque <nils.creque@smartstream-stp.com>
2018-08-03 14:13:08 +0000

    all things good like this

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace consoleapp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+  class Program
+  {
+      static void Main(string[] args)
+      {
+          Console.WriteLine("Hello World!");
+      }
+  }
 }
